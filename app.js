885f32b0de2f5cc0d12eddea7aca704cd0fbc48f
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2022-06-23 21:30:57 +0330

    Refactor Routes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 app.use(express.json());
 const path=require('path');
 const staticPath=path.join(__dirname,"public");
-//test
 const {addExercise,showUserExercise,deleteUserExercise,editUserExercsie} = require('./controllers/exerciseController');
 
 // ============= Twig ===========
@@ -26,17 +25,12 @@ const registerRoute=require('./routes/register');
 const loginRoute=require('./routes/login');
 const exerciseRoute=require('./routes/exercise');
 const mainRoute=require('./routes/main');
-const { hashID, hashPass } = require('./utils/hash');
 
 
 // =============== Routes Use ==============
 app.use('/',indexRoute);
 app.use('/register',registerRoute);
 app.use('/login',loginRoute);
-//app.use('/exercise',exerciseRoute);
+app.use('/exercise',exerciseRoute);
 app.use('/main',mainRoute);
-//test
-app.post('/exercise/add',addExercise);
-app.post('/exercise/edit',editUserExercsie);
-app.post('/exercise/delete',deleteUserExercise)
-app.get('/exercise',showUserExercise)
+
